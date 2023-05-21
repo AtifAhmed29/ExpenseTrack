37ee4f6ddfd988150b68104e9d253d62fcd0c126
Atif Ahmed <showrovislam29@gmail.com>
2023-05-21 21:38:03 +0600

    find by one id modified

--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -18,7 +18,7 @@ async findAll(): Promise<Expenses[]> {
   async  getById(id ) {
  
     
-      return await this.expenseModel.find({_id:id.id});
+      return await this.expenseModel.findOne({_id:id.id});
     }
  async   getByDate(date):Promise<Expenses[]> {
         const transection= await this.expenseModel.find({date:{$gte:date.min,$lte:date.max}}).sort([['date',1]])
@@ -33,7 +33,7 @@ async findAll(): Promise<Expenses[]> {
         return createdExpense.save();
       }
    async editExpense(id,data){
-        return await this.expenseModel.updateOne({_id:id.id},{$set:{category:data.category, title:data.title,ammount:data.ammount}});
+        return await this.expenseModel.updateOne({_id:id.id},{$set:{category:data.category, title:data.title,ammount:data.ammount,date:data.date }});
     }
   async  deleteExpense(id){
         return  await this.expenseModel.deleteOne({_id:id.id});
