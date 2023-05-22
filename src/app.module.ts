25f2efa3d6a9d1d41108164f7bef8b12978da3f5
Atif Ahmed <showrovislam29@gmail.com>
2023-05-22 22:28:48 +0600

    build and push

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { expenseMoudle } from './expense/expense.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'),expenseMoudle],
+  imports: [MongooseModule.forRoot('mongodb://mongo:Iz7Dvski7V2VZIP3W3Lv@containers-us-west-37.railway.app:7248'),expenseMoudle],
   controllers: [AppController],
   providers: [AppService],
 })
